2c213894ab323c406615785e42d2377ed825f23f
Antoine Dabonneville <antoine.dabonneville@pm.me>
2022-05-20 12:50:08 +0200

    add last update - not changing when build

--- pages/Campuses.js
+++ pages/Campuses.js
@@ -1,24 +1,30 @@
 import Layout from '../components/Layout';
 import { getCampusesFromDb } from '../db';
 
-function Campuses({ results }) {
+function Campuses({ results, lastUpdate }) {
+  // const generationDate = new Date();
   return (
     <Layout>
-      <h2>La liste des campus : </h2>
-      <div>
+      <h2>La liste des campus :</h2>
+      <div className='campusList'>
         {results.map((campus) => (
-          <div>{campus.name}</div>
+          <div className='campusItem' key={campus.id}>
+            {campus.name}
+          </div>
         ))}
       </div>
+      <div className='lastUpdate'>Last update : {lastUpdate}</div>
     </Layout>
   );
 }
 
 export default Campuses;
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const results = await getCampusesFromDb();
+  const generationDate = new Date();
+  const lastUpdate = generationDate.toString();
   return {
-    props: { results },
+    props: { results, lastUpdate },
   };
 }
--- pages/showRequest.js
+++ pages/showRequest.js
@@ -1,21 +1,20 @@
-import { string } from 'prop-types';
 import Layout from '../components/Layout';
 import getRawBody from 'raw-body';
+import { promisify } from 'util';
 
 function showRequest({ bodyString, headers, query }) {
   return (
     <Layout>
-      <ul>
-        <li>
-          headers : <pre>{JSON.stringify(headers)}</pre>
-        </li>
-        <li>
-          query : <pre>{JSON.stringify(query)}</pre>
-        </li>
-        <li>
-          body : <pre>{JSON.stringify(bodyString)}</pre>
-        </li>
-      </ul>
+      <div>
+        headers : <pre>{JSON.stringify(headers)}</pre>
+      </div>
+
+      <div>
+        query : <pre>{JSON.stringify(query)}</pre>
+      </div>
+      <div>
+        body : <pre>{JSON.stringify(bodyString)}</pre>
+      </div>
     </Layout>
   );
 }
@@ -27,14 +26,19 @@ export async function getServerSideProps(context) {
     'Cache-Control',
     'public, s-maxage=10, stale-while-revalidate=59'
   );
-
-  const body = await getRawBody(context.req);
-  console.log(context.req.method, body.toString('utf-8'));
-
   const headers = context.req.headers;
   const query = context.query;
+  let bodyString = '';
+  if (context.req.method === 'GET') {
+    bodyString = 'There is no body in GET request';
+  }
 
-  const bodyString = body.toString('utf-8');
+  if (context.req.method === 'POST') {
+    const body = await getRawBody(context.req);
+    // console.log(context.req.method, body.toString('utf-8'));
+    bodyString = body.toString('utf-8');
+  }
+
   return {
     props: { bodyString, headers, query },
   };
